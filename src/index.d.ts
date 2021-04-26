745e216ad5428e89c19dc1dc154c80d338b34233
felipe torres <felipe.torressepulveda@gmail.com>
2021-04-25 22:59:16 -0700

    lint and checks

---
+++ src/index.d.ts
@@ -0,0 +1,3 @@
+import { YeetGifSettings } from "./types";
+export declare const yeetGif: (inputPath: string, outputPath: string, settings: YeetGifSettings) => Promise<string>;
+//# sourceMappingURL=index.d.ts.map
